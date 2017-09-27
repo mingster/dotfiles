001a430acf54297dca20e6635e4a463f5bed227d
mingster <mingster.tsai@gmail.com>
2017-09-28 01:44:32 +0800

    script adjustment

--- bin/brewupdate.sh
+++ bin/brewupdate.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-sudo chown -R $(whoami):admin /usr/local
+
+#no longer needed in high sierra
+#sudo chown -R $(whoami):admin /usr/local
+
 brew update && brew upgrade
 brew cleanup
 mas upgrade
--- bin/emptyTrash.sh
+++ bin/emptyTrash.sh
@@ -3,4 +3,10 @@ sudo -v
 sudo rm -rf ~/.Trash/*
 sudo rm -rf /Volumes/MacHD/.Trashes/*
 sudo rm -rf /Volumes/data3/.Trashes/*
-sudo rm -rf /Volumes/backip_3tb/.Trashes/*
+#sudo rm -rf /Volumes/backip_3tb/.Trashes/*
+
+find . -name \.AppleDouble -exec rm -rf {} \;
+
+# in linux
+#find . -name ".AppleDouble" -delete
+#find . \( -name '.AppleDouble' -or -name '._*' \) -delete -print
