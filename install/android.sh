3ef39660d722575e803baa8f32cad9a90cfe69f4
Ming Tsai <mingster.tsai@gmail.com>
2018-08-26 12:12:03 +0800

    update software list

--- install/android.sh
+++ install/android.sh
@@ -20,21 +20,37 @@ brew update
 
 # Install Java
 #brew tap caskroom/cask
+
 brew cask install java8
-#brew cask install java
+brew cask install java
+brew install ant
+brew install maven
+brew install gradle
 
 #Manage Java enviornment - http://www.jenv.be
 brew install jenv
 
-#brew install Caskroom/cask/android-sdk
-brew cask install --appdir="/Applications/_dev" android-studio
-brew cask install --appdir="/Applications/_dev" eclipse-java
-brew cask install --appdir="/Applications/_dev" intellij-idea-ce
-
+brew tap homebrew/cask
+brew cask install android-sdk
 
 #############################################
 # https://gist.github.com/patrickhammond/4ddbe49a67e5eb1b9c03
 #############################################
+
+# Update environment variables:
+export ANT_HOME=/usr/local/opt/ant
+export MAVEN_HOME=/usr/local/opt/maven
+export GRADLE_HOME=/usr/local/opt/gradle
+
+export PATH=$ANT_HOME/bin:$PATH
+export PATH=$MAVEN_HOME/bin:$PATH
+export PATH=$GRADLE_HOME/bin:$PATH
+
+#brew install Caskroom/cask/android-sdk
+brew cask install --appdir="/Applications/_dev" android-studio
+#brew cask install --appdir="/Applications/_dev" eclipse-java
+#brew cask install --appdir="/Applications/_dev" intellij-idea-ce
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
