6de3d06a70c57d3168a43e304f1eddca1193b320
Ming Tsai <mingster.tsai@gmail.com>
2020-12-16 22:15:56 +0800

    Update brew_software.sh

--- install/brew_software.sh
+++ install/brew_software.sh
@@ -1,49 +1,49 @@
 
-#brew cask install --appdir="~/Applications" xquartz
-brew cask install --appdir="/Applications/Utilities" onyx
+#brew install --cask --appdir="~/Applications" xquartz
+brew install --cask --appdir="/Applications/Utilities" onyx
 
 
 #brew install git
-#brew cask install virtualbox
-#brew cask install vagrant
+#brew install --cask virtualbox
+#brew install --cask vagrant
 # Install Docker, which requires virtualbox
 brew install docker
 brew install boot2docker
 
 
-brew cask install --appdir="/Applications/_dev" 0xed
-brew cask install --appdir="/Applications/_dev" eclipse-jee
-brew cask install --appdir="/Applications/_dev" jd-gui
-brew cask install --appdir="/Applications/_dev" dash
-brew cask install wireshark
+brew install --cask --appdir="/Applications/_dev" 0xed
+brew install --cask --appdir="/Applications/_dev" eclipse-jee
+brew install --cask --appdir="/Applications/_dev" jd-gui
+brew install --cask --appdir="/Applications/_dev" dash
+brew install --cask wireshark
 
 # Misc casks
-brew cask install aerial #appletv screen saver
-brew cask install welly #ptt telnet client
-#brew cask install evernote
-#brew cask install 1password
-#brew cask install gimp
-#brew cask install inkscape
-brew cask install skitch
+brew install --cask aerial #appletv screen saver
+brew install --cask welly #ptt telnet client
+#brew install --cask evernote
+#brew install --cask 1password
+#brew install --cask gimp
+#brew install --cask inkscape
+brew install --cask skitch
 
 #https://www.torproject.org/docs/tor-doc-osx.html.en
 #brew install tor
-brew cask install tor-browser
+brew install --cask tor-browser
 
 #Remove comment to install LaTeX distribution MacTeX
-#brew cask install mactex
+#brew install --cask mactex
 
 # Install av staff
 mkdir /Applications/_av
 
-brew cask install --appdir="/Applications/_av" aegisub
-brew cask install --appdir="/Applications/_av" handbrake
-brew cask install --appdir="/Applications/_av" kid3
-brew cask install --appdir="/Applications/_av" jubler
-brew cask install --appdir="/Applications/_av" vox
-brew cask install --appdir="/Applications/_av" iina
-brew cask install --appdir="/Applications/_av" xld
-#brew cask install --appdir="/Applications/_av" get-lyrical
+brew install --cask --appdir="/Applications/_av" aegisub
+brew install --cask --appdir="/Applications/_av" handbrake
+brew install --cask --appdir="/Applications/_av" kid3
+brew install --cask --appdir="/Applications/_av" jubler
+brew install --cask --appdir="/Applications/_av" vox
+brew install --cask --appdir="/Applications/_av" iina
+brew install --cask --appdir="/Applications/_av" xld
+#brew install --cask --appdir="/Applications/_av" get-lyrical
 brew install mp3splt
 
 #ffmpeg with aac/mp4 support
@@ -52,7 +52,7 @@ brew install libvpx
 brew install ffmpeg --with-fdk-aac --with-tools --with-sdl2 --with-freetype --with-libass --with-libqavi --with-libvorbis --with-libvpx --with-opus --with-x265
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
-#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+#brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
 # install packages listed in Brewfile
 #### review Brewfile and manaully pick the app to install ###
@@ -64,4 +64,4 @@ brew install ffmpeg --with-fdk-aac --with-tools --with-sdl2 --with-freetype --wi
 
 #google-chrome-canary
 brew tap homebrew/cask-versions
-brew cask install google-chrome-canary
+brew install --cask google-chrome-canary
