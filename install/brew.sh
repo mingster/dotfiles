05a94a8ffc125c265e86500c561123cedd0ec415
mingster <mingster@users.noreply.github.com>
2024-01-23 22:23:13 +0800

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -39,7 +39,9 @@ brew install --cask skype
 #brew install --cask slack
 #brew install --cask dropbox
 brew install --cask megasync
-brew install --cask onebox
+brew install --cask google-drive
+#brew install --cask onedrive
+
 #brew install --cask numi
 #brew install --cask alfred
 #brew install --cask keyboard-maestro
