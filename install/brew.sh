15ebdf3328d0eb9505c08d7e1b9a9130d97d842d
Ming Tsai <mingster.tsai@gmail.com>
2024-04-17 23:49:10 +0800

    change to fish shell

--- install/brew.sh
+++ install/brew.sh
@@ -26,12 +26,30 @@ brew update && brew upgrade
 
 # Install essential binaries.
 brew install mas
+brew install neofetch
+brew install fish
+
+#mkdir ~/.config/fish
+#ln ~/GitHub/dotfiles/.config/fish $HOME/.config/fish
+
+brew install nvim
+# lazyvim
+# required
+mkdir ~/.config
+# optional but recommended
+mkdir ~/.local/share/nvim
+mkdir ~/.local/state/nvim
+mkdir ~/.cache/nvim
+
+ln -s ~/GitHub/dotfiles/.config/nvim $HOME/.config/nvim
+
+#brew install stow
 
 #1176895641  Spark – Email App by Readdle
 #mas install 1176895641
 
 # essential cask apps
-brew install stow
+
 #brew install borgbackup
 
 brew install --cask iterm2
@@ -65,21 +83,38 @@ brew cleanup && brew doctor
 #oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ../.zshrc ~/
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+#cp ../.zshrc ~/
 
 # font for powerlevel9k theme
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 # powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # powerlevel9k theme
 #git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 #zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 #brew install zsh
 #use mac built-in zsh
-chsh -s $(which zsh)
+#chsh -s $(which zsh)
+
+
+# add fish to system shell
+su
+echo $(which fish) >> /etc/shells
+
+# change shell to fish
+chsh -s $(which fish)
+
+# install fisher
+## enter fish shell
+fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# install shell theme
+fisher install IlanCosman/tide@v6
