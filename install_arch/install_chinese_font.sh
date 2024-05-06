f29e80e797d71e2dec81d54f506676218653c586
mingster <mingster@users.noreply.github.com>
2024-05-06 22:31:30 +0800

    chinese font install

---
+++ install_arch/install_chinese_font.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## install noto chinese fonts
+sudo pacman -S noto-fonts
+
+cd /tmp && \
+git clone https://aur.archlinux.org/noto-fonts-tc.git  && \
+cd noto-fonts-tc && \
+makepkg -Acs && \
+sudo pacman -U noto-fonts-tc-2:20201206-1-any.pkg.tar.zst
+
+
+cd /tmp && \
+git clone https://aur.archlinux.org/noto-fonts-sc.git && \
+cd noto-fonts-sc && makepkg -Acs && \
+sudo pacman -U noto-fonts-sc-2:20210430-2-any.pkg.tar.zst
+
+
+## input method
+sudo pacman -Syu fcitx fcitx-googlepinyin fcitx-im fcitx-configtool
