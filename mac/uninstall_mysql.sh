16d776714d26b9e7d434e56339a0c442197cf3ac
mingster <mingster@users.noreply.github.com>
2024-06-04 10:18:03 +0800

    uninstall mysql scripit

---
+++ mac/uninstall_mysql.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+
+#
+# https://myapollo.com.tw/blog/install-mysql-using-homebrew/
+# https://www.geeksforgeeks.org/how-to-install-and-configure-mysql-on-arch-based-linux-distributionsmanjaro/
+# https://gist.github.com/Foadsf/b351fe7686de19a4c91d3e0b4c91080a
+
+# this will install mariadb
+brew install mysql
+# brew install mycli
+
+brew services start mysql
+
+
+# this only works after you allow background access
+brew services restart mysql
+
+# set root password
+mysql_secure_installation
+
+# to create new db user
+# https://docs.rackspace.com/docs/create-a-new-user-and-grant-permissions-in-mysql
+
+# mysql -u root
+# create database tutorialdb;
+# create user 'tutorialuser'@'localhost' identified by 'tutorialuser';
+# grant all on tutorialdb.* to 'tutorialuser'@'localhost';
+# exit
+
+# test the new user
+# mysql -u tutorialuser -p
+
+# sudo mysqld_safe --skip-grant-tables;
+# GRANT ALL PRIVILEGES ON *.* TO root@localhost WITH GRANT OPTION;
