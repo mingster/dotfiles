6142f60a1eacdd9975bad7282a3866792f2e1465
mingster <mingster@users.noreply.github.com>
2024-06-04 12:31:37 +0800

    mysql shit

--- mac/install_mysql.sh
+++ mac/install_mysql.sh
@@ -6,25 +6,31 @@
 # https://www.geeksforgeeks.org/how-to-install-and-configure-mysql-on-arch-based-linux-distributionsmanjaro/
 # https://gist.github.com/Foadsf/b351fe7686de19a4c91d3e0b4c91080a
 
-# this will install mariadb
 brew install mysql
 # brew install mycli
 
-brew services start mysql
-
+# set root password
+mysql_secure_installation
 
 # this only works after you allow background access
 brew services restart mysql
 
-# set root password
-mysql_secure_installation
+# brew services start mysql
+
+# thr process
+# /usr/local/opt/mysql/bin/mysqld --basedir=/usr/local/opt/mysql --datadir=/usr/local/var/mysql
+# --plugin-dir=/usr/local/opt/mysql/lib/plugin --log-error=niteking.local.err --pid-file=niteking.local.pid
+
+## result:
+#==> /usr/local/Cellar/mysql/8.3.0_1/bin/mysqld --initialize-insecure --user=mtsai --basedir=/usr/local/Cellar/mysql/8.3.0_1 --datadir=/usr/local/var/mysql --tmpdir=/tmp
+
 
 # to create new db user
 # https://docs.rackspace.com/docs/create-a-new-user-and-grant-permissions-in-mysql
 
 # mysql -u root
 # create database tutorialdb;
-# create user 'tutorialuser'@'localhost' identified by 'tutorialuser';
+# create user 'dev'@'localhost' identified by 'dev4fun!';
 # grant all on tutorialdb.* to 'tutorialuser'@'localhost';
 # exit
 
--- mac/uninstall_mysql.sh
+++ mac/uninstall_mysql.sh
@@ -1,35 +1,16 @@
 
 #!/bin/bash
 
-#
-# https://myapollo.com.tw/blog/install-mysql-using-homebrew/
-# https://www.geeksforgeeks.org/how-to-install-and-configure-mysql-on-arch-based-linux-distributionsmanjaro/
-# https://gist.github.com/Foadsf/b351fe7686de19a4c91d3e0b4c91080a
 
-# this will install mariadb
-brew install mysql
-# brew install mycli
+brew remove mysql
+brew cleanup
 
-brew services start mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
 
+sudo rm -rf /usr/local/etc/my.cnf.*
 
-# this only works after you allow background access
-brew services restart mysql
-
-# set root password
-mysql_secure_installation
-
-# to create new db user
-# https://docs.rackspace.com/docs/create-a-new-user-and-grant-permissions-in-mysql
-
-# mysql -u root
-# create database tutorialdb;
-# create user 'tutorialuser'@'localhost' identified by 'tutorialuser';
-# grant all on tutorialdb.* to 'tutorialuser'@'localhost';
-# exit
-
-# test the new user
-# mysql -u tutorialuser -p
-
-# sudo mysqld_safe --skip-grant-tables;
-# GRANT ALL PRIVILEGES ON *.* TO root@localhost WITH GRANT OPTION;
+# data dir
+sudo rm -rf /usr/local/var/mysql
