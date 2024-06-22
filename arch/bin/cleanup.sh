90725bee3a288fee656ad8f5b9faaceca486946d
mingster <mingster@users.noreply.github.com>
2024-06-22 10:27:37 +0800

    no confirm

--- arch/bin/cleanup.sh
+++ arch/bin/cleanup.sh
@@ -3,4 +3,8 @@
 sudo journalctl --flush --rotate --vacuum-time=1s
 sudo journalctl --user --flush --rotate --vacuum-time=1s
 sudo journalctl --vacuum-size=500M
-sudo pacman -Scc
+
+# https://wiki.archlinux.org/title/Pacman#Cleaning_the_package_cache
+
+sudo pacman -Sc --noconfirm
+sudo pacman -Scc --noconfirm
