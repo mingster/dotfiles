9d6bfe773e43de0d988c99c4d1fd7553b98ba636
mingster <mingster@users.noreply.github.com>
2024-09-07 15:17:51 +0800

    brew upgrade --cask --greedy

--- mac/bin/update.sh
+++ mac/bin/update.sh
@@ -3,13 +3,15 @@
 #no longer needed in high sierra
 #sudo chown -R $(whoami):admin /usr/local
 
-brew update && brew upgrade && brew cleanup
+brew update
+brew upgrade
+brew upgrade --cask --greedy
 
 #brew update && brew upgrade
-
-brew upgrade --cask
 #brew cleanup
 mas upgrade
+
+brew cleanup
 brew doctor
 
 rm -rf ~/Library/Caches/Homebrew/downloads/*
