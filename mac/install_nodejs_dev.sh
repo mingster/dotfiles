942aab81dc68f11ebffcdd372db75212ca0db657
mingster <mingster@users.noreply.github.com>
2024-09-27 15:28:48 +0800

    Update install_nodejs_dev.sh

--- mac/install_nodejs_dev.sh
+++ mac/install_nodejs_dev.sh
@@ -25,11 +25,11 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf nodejs update-nodebuild
 
 asdf list all nodejs
-asdf install nodejs 20.16.0
-asdf global nodejs 20.16.0
+asdf install nodejs 20.17.0
+asdf global nodejs 20.17.0
 
 #npm install -g npm@latest
 
-asdf shell nodejs 20.16.0
+asdf shell nodejs 20.17.0
 corepack enable
 asdf reshim nodejs
