c52af666e224b4f7cb603b88e0fb01811dbe2bf3
mingster <mingster@users.noreply.github.com>
2025-04-28 23:41:53 +0800

    asdf for arch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,22 @@ switch (uname)
         set -gx CPPFLAGS $CPPFLAGS "-I/usr/lib/jvm/default/include"
 
         # activate asdf
-        source /opt/asdf-vm/asdf.fish
+        # ASDF configuration code
+        if test -z $ASDF_DATA_DIR
+            set _asdf_shims "$HOME/.asdf/shims"
+        else
+            set _asdf_shims "$ASDF_DATA_DIR/shims"
+        end
+
+        asdf completion fish > ~/.config/fish/completions/asdf.fish
+
+        # Do not use fish_add_path (added in Fish 3.2) because it
+        # potentially changes the order of items in PATH
+        if not contains $_asdf_shims $PATH
+            set -gx --prepend PATH $_asdf_shims
+        end
+        set --erase _asdf_shims
+
 
         # gradle
         #set -gx GRADLE_HOME /usr/bin/gradle
--- arch/install_node.sh
+++ arch/install_node.sh
@@ -1,45 +1,16 @@
 #!/usr/bin/env bash
 
+#git clone https://aur.archlinux.org/asdf-vm.git && cd asdf-vm && makepkg -si
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-brew list | grep -iE 'node'
-for pkg in $(brew list | grep -iE 'node'); do brew --ignore-dependencies uninstall $pkg; done
-
+yay -S asdf-vm
 
 # https://blog.logrocket.com/manage-node-js-versions-using-asdf/
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
-asdf nodejs update-nodebuild
-
 asdf list all nodejs
 
-asdf nodejs resolve lts --latest-available
-
-asdf install nodejs 20.18.0
-asdf global nodejs 20.18.0
-asdf shell nodejs 20.18.0
-
-npm install -g npm@latest
-
-corepack enable
-
-npm install -g yarn
-corepack prepare yarn@latest --activate
+asdf install nodejs 22.14.0
+asdf set -u nodejs 22.14.0
 
 asdf reshim nodejs
 curl -fsSL https://bun.sh/install | bash
