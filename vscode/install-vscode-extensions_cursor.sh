88ef3f4c832c3c9fafdcb206b1496e5593081ff2
mingster <mingster@users.noreply.github.com>
2025-05-11 09:55:37 +0800

    feat:🎸 : renew react extensions

--- vscode/install-vscode-extensions_cursor.sh
+++ vscode/install-vscode-extensions_cursor.sh
@@ -4,74 +4,29 @@
 # -------------------
 # curl -s https://raw.githubusercontent.com/mingster/dotfiles/master/vscode/install-vscode-extensions.sh | /bin/bash
 
-# Visual Studio Code :: Package list
-# get this list by: code --list-extensions
+# cursor :: Package list
+# get this list by: cursor --list-extensions
 pkglist=(
-#alefragnani.project-manager
-anteprimorac.html-end-tag-labels
-#austenc.tailwind-docs
-benjaminbenais.copilot-theme
 biomejs.biome
 bradlc.vscode-tailwindcss
-cesium.gltf-vscode
-christian-kohler.npm-intellisense
-christian-kohler.path-intellisense
-ckolkman.vscode-postgres
-codeium.codeium
-codezombiech.gitignore
-#cweijan.dbclient-jdbc
-#cweijan.vscode-mysql-client2
-#cweijan.vscode-redis-client
 davidanson.vscode-markdownlint
-#dbaeumer.vscode-eslint
-#donjayamanne.githistory
+dbaeumer.vscode-eslint
 dotjoshjohnson.xml
-#dsznajder.es7-react-js-snippets
-waderyan.gitblame
-#eamodio.gitlens
-editorconfig.editorconfig
 exodiusstudios.comment-anchors
-formulahendry.auto-close-tag
-formulahendry.auto-rename-tag
 foxundermoon.shell-format
-github.codespaces
-#github.copilot
-#github.copilot-chat
 github.github-vscode-theme
-github.remotehub
-github.vscode-github-actions
-github.vscode-pull-request-github
-heybourn.headwind
-jasonnutter.search-node-modules
 josee9988.minifyall
 maptz.regionfolder
-#mgmcdermott.vscode-language-babel
-#mike-co.import-sorter
 mikestead.dotenv
-mongodb.mongodb-vscode
-mrmlnc.vscode-scss
-#ms-vscode-remote.remote-containers
-#ms-vscode.azure-repos
-ms-vscode.remote-repositories
-mtxr.sqltools
-#mtxr.sqltools-driver-mysql
-mtxr.sqltools-driver-pg
+ms-mssql.data-workspace-vscode
+ms-mssql.mssql
+ms-mssql.sql-bindings-vscode
+ms-mssql.sql-database-projects-vscode
 pkief.material-icon-theme
 pkief.material-product-icons
-planbcoding.vscode-react-refactor
 prisma.prisma
-rangav.vscode-thunder-client
-redhat.vscode-xml
 richie5um2.vscode-sort-json
 s-nlf-fh.glassit
-sburg.vscode-javascript-booster
-shd101wyy.markdown-preview-enhanced
-sibiraj-s.vscode-scss-formatter
-usernamehw.errorlens
-#visualstudioexptteam.intellicode-api-usage-examples
-vsls-contrib.gistfs
-yseop.vscode-yseopml
-zignd.html-css-class-completion
 )
 
 for i in ${pkglist[@]}; do
