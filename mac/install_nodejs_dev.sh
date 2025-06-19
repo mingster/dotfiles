887b44e1b3069df0a1e72c5ded8bc701bbd3d5d3
mingster <mingster@users.noreply.github.com>
2025-06-19 11:26:02 +0800

    Update install_nodejs_dev.sh

--- mac/install_nodejs_dev.sh
+++ mac/install_nodejs_dev.sh
@@ -30,8 +30,8 @@ asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf list all nodejs | wc -l
 
 asdf install nodejs 22.14.0
-asdf global nodejs 22.14.0
-asdf shell nodejs 22.14.0
+#asdf global nodejs 22.14.0
+#asdf shell nodejs 22.14.0
 asdf set -u nodejs 22.14.0
 
 
